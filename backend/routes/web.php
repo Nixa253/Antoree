fe351c300c4f52818ad84a8e282e7bc892365ef3
Nixa253 <luutoannghia2003@gmail.com>
2025-06-09 09:44:18 +0700

    migrate db

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/run-migrate', function () {
+    try {
+        \Artisan::call('migrate', ['--force' => true]);
+        return 'Đã migrate thành công!';
+    } catch (\Exception $e) {
+        return 'Lỗi: ' . $e->getMessage();
+    }
+});
